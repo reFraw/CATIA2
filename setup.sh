8f766fd165e62bf1f278c626f5f81481f0bf4883
Fabrizio Ravelli <105207472+reFraw@users.noreply.github.com>
2022-11-08 18:15:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "[*] STARTING CATIA2 INSTALLATION."
 pip install -q -r CORE/requirements/requirements.txt
 mv CORE .CATIA
 cp -r .CATIA /home/$USER/
-echo "alias CATIA2='python3 /home/$USER/.CATIA/main.py'"
+echo "alias CATIA2='python3 /home/$USER/.CATIA/main.py'" >> /home/$USER/.bashrc
 
 echo "[#] CATIA2 INSTALLATION FINISHED."
 
