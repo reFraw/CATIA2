787bb31ed977518f24e6fcd68c1250d22b9a5ceb
Fabrizio Ravelli <105207472+reFraw@users.noreply.github.com>
2022-11-08 16:00:15 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "[*] STARTING CATIA2 INSTALLATION."
+
+pip install -q -r CORE/requirements/requirements.txt
+mv CORE .CATIA
+cp -r .CATIA ..
+echo "alias CATIA2='python3 /home/$USER/.CATIA/main.py'"
+
+echo "[#] CATIA2 INSTALLATION FINISHED."
+
+cd ..
+rm -rf CATIA2
